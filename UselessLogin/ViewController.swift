46a9dc25575a9a58657bfeb715164a317a03e335
Yoltic Cervantes Galeana <yoltic.cervantes@dotrs.com.mx>
2019-09-16 05:58:23 -0500

    Initial Commit

---
+++ UselessLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UselessLogin
+//
+//  Created by Yoltic Cervantes Galeana on 9/16/19.
+//  Copyright © 2019 Yoltic Cervantes Galeana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
